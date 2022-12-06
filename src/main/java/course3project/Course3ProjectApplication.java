295023ff80946248beb2b80e939343ef2d2b85d0
Ирина <mi.irene97@gmail.com>
2022-12-06 13:22:43 +0300

    Initial commit

---
+++ src/main/java/course3project/Course3ProjectApplication.java
@@ -0,0 +1,13 @@
+package course3project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course3ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course3ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/course3project/Course3ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package course3project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course3ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
